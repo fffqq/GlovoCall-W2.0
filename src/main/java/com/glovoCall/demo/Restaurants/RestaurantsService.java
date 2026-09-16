f857f1a53e1223e91222887d884d9bae3fe4771c
Valerii <argon3300@gmail.com>
2026-09-16 15:52:08 +0300

    restaurant validation

--- src/main/java/com/glovoCall/demo/Restaurants/RestaurantsService.java
+++ src/main/java/com/glovoCall/demo/Restaurants/RestaurantsService.java
@@ -24,6 +24,13 @@ public class RestaurantsService {
                 .toList();
     }
     public void CreateRestaurant(Restaurant restaurant){
+        if(restaurant == null){
+            throw new IllegalArgumentException("restaurant is null. IDI NAHUI SUKA");
+        }
+
+        if (RestaurantRepo.existsByNameAndAddress(restaurant.getName(), restaurant.getAddress())) {
+            throw new IllegalArgumentException("restaurant  already exist. IDI NAHUI");
+        }
         RestaurantRepo.save(restaurant);
     }
 
